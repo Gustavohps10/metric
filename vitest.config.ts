89074e6c0caf67e0b8d1d6cba05d0121f3b187e2
Gustavohps10 <gustavoh.santos735@gmail.com>
2026-04-18 21:25:33 -0300

    ci: update alias import from  vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  resolve: {
-    tsconfigPaths: true,
-  },
+  // resolve: {
+  //   tsconfigPaths: true, Isso funciona localmente porem quebra na pipeline do Github, rever repois
+  // },
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
